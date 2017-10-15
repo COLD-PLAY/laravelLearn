ef40d8d3c5920bca8759997b5738767428a6513c
廖舟 <mi_xi_shi_zi@163.com>
2017-10-16 00:08:36 +0800

    add hello

---
+++ hello/routes/channels.php
@@ -0,0 +1,16 @@
+
+
+    return (int) $user->id === (int) $id;
+*/
+/*
+<?php
+Broadcast::channel('App.User.{id}', function ($user, $id) {
+|
+|
+| application supports. The given channel authorization callbacks are
+| Broadcast Channels
+| Here you may register all of the event broadcasting channels that your
+| used to check if an authenticated user can listen to the channel.
+|--------------------------------------------------------------------------
+|--------------------------------------------------------------------------
+});
